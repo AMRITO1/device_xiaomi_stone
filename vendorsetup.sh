86ca693dcd22797eee0c053e7f7126fc7e12a2e6
Taki / <amrito@myyahoo.com>
2024-02-04 19:05:09 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,7 +4,7 @@ git clone --depth=1 https://github.com/Stone-Trees/vendor_xiaomi.git -b lineage-
 
 git clone --depth=1 https://github.com/Stone-Trees/kernel_xiaomi_sm6375.git -b lineage-20 kernel/xiaomi/sm6375
 
-git clone --depth=1 https://github.com/Stone-Trees/xiaomi_stone-kernel.git device/xiaomi/stone-kernel
+git clone --depth=1 https://github.com/Amrito-Projects/device_xiaomi_stone-kernel.git -b 13  device/xiaomi/stone-kernel
 
 git clone --depth=1 https://github.com/stone-devs/hardware_xiaomi hardware/xiaomi
 
